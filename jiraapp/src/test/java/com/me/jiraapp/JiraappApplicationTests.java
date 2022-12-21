f7dab601e8253aee1bf79c611bd74357d973badd
Abhishek venkata <abhishekvenkata.199715@gmail.com>
2022-12-21 13:21:35 -0600

    Upload github project

---
+++ jiraapp/src/test/java/com/me/jiraapp/JiraappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.jiraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
